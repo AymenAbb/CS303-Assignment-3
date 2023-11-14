8522fad76a4364269bd0202027732f9ba7e69d4d
Aymen <123590232+AymenAbb@users.noreply.github.com>
2023-11-13 19:07:39 -0600

    Update Infix_To_Postfix.h

--- Part 1/Infix_To_Postfix.h
+++ Part 1/Infix_To_Postfix.h
@@ -1,24 +1,24 @@
 #ifndef INFIX_TO_POSTFIX_H_
 #define INFIX_TO_POSTFIX_H_
 #endif
-
+using namespace std;
 #include "stack.h"
 #include <string>
 
 class Infix_To_Postfix {
 public:
-  std::string convert(const std::string &infix);
+  string convert(const string &infix);
 
 private:
   stackNS::stack<char> operator_stack;
-  std::string postfix;
+  string postfix;
   void process_operator(char op);
   int precedence(char op) const;
   bool is_operator(char ch) const;
-  static const std::string OPERATORS;
+  static const string OPERATORS;
   static const int PRECEDENCE[];
   bool open_parentheses(char ch) const;
   bool closed_parentheses(char ch) const;
-  static const std::string OPEN;
-  static const std::string CLOSE;
+  static const string OPEN;
+  static const string CLOSE;
 };
